43e5be368f93ec93c12510b53bf59da544d17b6b
kitare17 <hoctapak@gmail.com>
2024-05-04 21:02:37 +0700

    header footer

---
+++ src/app/landing-page/page.tsx
@@ -0,0 +1,10 @@
+
+import LandingPage from "@/app/landing-page/LandingPage";
+import AppAppBar from "@/app/landing-page/components/AppAppBar";
+
+const landingPage = () => {
+    return(
+        <AppAppBar/>
+    )
+}
+export default landingPage;
