f8fd7d1744005cc81c798af76be91ea224def55b
kitare17 <hoctapak@gmail.com>
2024-05-19 10:50:38 +0700

    fix login

--- src/app/store/action/auth.ts
+++ src/app/store/action/auth.ts
@@ -1,65 +1,61 @@
 import * as Types from "../../constant/ActionType";
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import {createAsyncThunk} from "@reduxjs/toolkit";
 import axios from "axios";
-import { UserInterface } from "@/app/interface/userinterface";
-import { LoginInterface } from "@/app/interface/LoginInterface";
-import { RegisterInterface } from "@/app/interface/RegisterInterface";
+import {UserInterface} from "@/app/interface/userinterface";
+import {LoginInterface} from "@/app/interface/LoginInterface";
+import {RegisterInterface} from "@/app/interface/RegisterInterface";
 
 export const fetchLogin = createAsyncThunk(
-  Types.LOGIN,
-  async (userLogin: LoginInterface) => {
-    try {
-      const response = await axios.post("http://localhost:3001/users/login", {
-        email: userLogin.email,
-        password: userLogin.password,
-      });
+    Types.LOGIN,
+    async (userLogin: LoginInterface, {rejectWithValue}) => {
+        try {
+            const response = await axios.post("http://localhost:3001/users/login", {
+                email: userLogin.email,
+                password: userLogin.password,
+            });
 
-      const data: LoginInterface = response.data;
-      window.localStorage.setItem("authnRes", JSON.stringify(data));
-      return data;
-    } catch (error) {
-      console.log("error", error);
-      return error;
+            const data: LoginInterface = response.data;
+            window.localStorage.setItem("authnRes", JSON.stringify(data));
+            return data;
+        } catch (error) {
+
+            // @ts-ignore
+            const errorData = error as AxiosError;
+            console.log("Error data",errorData.response?.data)
+            return rejectWithValue({data: errorData.response?.data});
+        }
     }
-  }
 );
 
 export const fetchLogout = createAsyncThunk(Types.LOGOUT, async () => {
-  try {
-    const response = await axios.post("http://localhost:3001/users/logout");
-    return response.data;
-  } catch (error) {
-    console.log("Error: " + Types.LOGOUT);
-    return error;
-  }
-});
-
-export const fetchRegister = createAsyncThunk(
-  Types.REGISTER,
-  async (userRegister: RegisterInterface) => {
     try {
-      const response = await axios.post(
-        "http://localhost:3001/users/register",
-        {
-          email: userRegister.email,
-          password: userRegister.password,
-          username: userRegister.username
-        }
-      );
-      
-      console.log("response.data", response.data)
-      if(!(response.data.statusMessage === "Error")) {
-        console.log("responaaaaaase.data", response.data)
-
-        const data: RegisterInterface = response.data;
-        return data;
-      }
-      return response.data;
+        const response = await axios.post("http://localhost:3001/users/logout");
+        console.log("dang xuat")
+        return response.data;
     } catch (error) {
-          // @ts-ignore
-      console.log("Error: " + error.payload.response.data.statusMessage);
-
-      return error;
+        console.log("Error: " + Types.LOGOUT);
+        return error;
     }
-  }
+});
+export const fetchRegister = createAsyncThunk(
+    Types.REGISTER,
+    async (userRegister: RegisterInterface, {rejectWithValue}) => {
+        try {
+            const response = await axios.post(
+                "http://localhost:3001/users/register",
+                {
+                    email: userRegister.email,
+                    password: userRegister.password,
+                    username: userRegister.username
+                }
+            );
+            const data: RegisterInterface = response.data;
+            return data;
+
+        } catch (error) {
+            // @ts-ignore
+            const errorData = error as AxiosError;
+            return rejectWithValue({data: errorData.response?.data});
+        }
+    }
 );
